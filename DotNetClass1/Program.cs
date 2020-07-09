7d404623f422b5349272b8cf1d1733946d4f52f9
Shari <shares_322@yahoo.com>
2020-07-08 21:52:40 -0400

    Fix conflict from local to remote

---
+++ DotNetClass1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DotNetClass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            var userInput = Console.ReadLine();
+            var isDecimal = decimal.TryParse(userInput, out decimal userInputAsDecimal);
+            
+            if (!isDecimal)
+            {
+                Console.WriteLine("Error - incorrect input - Try again");
+                return;
+            }
+            userInputAsDecimal += 0.5m;
+            Console.WriteLine(userInputAsDecimal);
+        }
+    }
+}
