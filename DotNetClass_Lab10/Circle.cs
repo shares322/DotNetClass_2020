fba3572be0651c34266857a9f2db3a1427f18a26
Shari <shares_322@yahoo.com>
2020-08-03 17:23:28 -0400

    Adding Lab 10

---
+++ DotNetClass_Lab10/Circle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetClass_Lab10
+{
+    class Circle
+    {
+        private int radius;
+
+        public double Radius { get; set; }
+
+
+        //constructor
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        //method
+        public double CalculateCircumference()
+        {
+            var circumference = 2 * Math.PI * Radius;
+            return circumference;
+        }
+
+        private string FormatNumber(double x)
+        {
+            var roundedInput = Math.Round(x, 2);
+            return roundedInput.ToString();
+            
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            var circumference = CalculateCircumference();
+            return FormatNumber(circumference);
+        }
+
+        //method
+        public double CalculateArea()
+        {
+            var area = Math.PI * (Radius * Radius);
+            return area;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            var area = CalculateArea();
+            return FormatNumber(area);
+        }
+    }
+}
---
+++ DotNetClass_Lab10/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DotNetClass_Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ansYes = true;
+            int count = 1;
+            Console.WriteLine("Welcome to the Circle Tester:");
+            Console.WriteLine();
+
+            do
+            {
+                Console.WriteLine("Please enter a radius: ");
+                var userInput = Console.ReadLine();
+
+                double aDouble;
+                if (!double.TryParse(userInput, out aDouble))
+                {
+                    Console.WriteLine("That was not a number.  Please try again.");
+                    Console.WriteLine();
+                }
+                else 
+                {
+                    var radius = double.Parse(userInput);
+                    var circle = new Circle(radius);
+
+                    Console.WriteLine($"Circumference: {circle.CalculateFormattedCircumference()}.");
+                    Console.WriteLine($"Area: {circle.CalculateFormattedArea()}.");
+
+                    Console.WriteLine("Would you like to continue? y/n: ");
+                    string continueAnswer = Console.ReadLine();
+                    char.TryParse(continueAnswer, out char contAnswer);
+
+                    if (contAnswer == 'y' || contAnswer == 'Y')
+                    {
+                        ansYes = true;
+                        count++;
+                    }
+                    else
+                    {
+                        ansYes = false;
+                    }
+                }
+ 
+            } while (ansYes == true);
+
+            int countLoops = count;
+            Console.WriteLine("Good-bye. You created " + countLoops + " Circle object(s).");
+        }
+        
+    }
+}
