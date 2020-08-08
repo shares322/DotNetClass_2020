d995fd66f67f6a2784671b0824c2bc7a380242c2
Shari <shares_322@yahoo.com>
2020-08-08 14:23:07 -0400

    Updated the Program.cs file - Lab11

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -6,48 +6,69 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
-        
+            bool ansYes = true;
             Console.WriteLine("Welcome to the Movie List Application!");
             Console.WriteLine("There are 100 movies in this list.");
             Console.WriteLine();
-            Console.WriteLine("What category are you interested in?: ");
-
-            var userInput = Console.ReadLine();
-
-            var isMovieList = Enum.TryParse(userInput, out MovieCategoryList movieCategoryList);
-            if (isMovieList)
+            do
             {
-                IMovie movie;
+                Console.WriteLine("What category are you interested in? Animated, Drama, Horror, or SciFi?: ");
 
-                switch (movieCategoryList)
+                var userInput = Console.ReadLine();
+
+                var isMovieList = Enum.TryParse(userInput, out MovieCategoryList movieCategoryList);
+                if (isMovieList)
                 {
-                    case MovieCategoryList.Animated:
-                        Console.WriteLine("You entered an Animated movie.");
-                        movie = new Animated();
-                        movie.Genre();
-                        break;
-                    case MovieCategoryList.Drama:
-                        Console.WriteLine("You entered a Drama movie.");
-                        movie = new Drama();
-                        movie.Genre();
-                        break;
-                    case MovieCategoryList.Horror:
-                        Console.WriteLine("You entered a Horror movie.");
-                        movie = new Horror();
-                        movie.Genre();
-                        break;
-                    case MovieCategoryList.SciFi:
-                        Console.WriteLine("You entered a SciFie movie.");
-                        movie = new SciFi();
-                        movie.Genre();
-                        break;
-                    default:
-                        break;
+                    IMovie movie;
+
+                    switch (movieCategoryList)
+                    {
+                        case MovieCategoryList.Animated:
+                            Console.WriteLine("You entered the Animated movie category. The list of movies are: ");
+                            movie = new Animated();
+                            movie.Genre();
+                            break;
+                        case MovieCategoryList.Drama:
+                            Console.WriteLine("You entered the Drama movie category. The list of movies are: ");
+                            movie = new Drama();
+                            movie.Genre();
+                            break;
+                        case MovieCategoryList.Horror:
+                            Console.WriteLine("You entered the Horror movie category. The list of movies are: ");
+                            movie = new Horror();
+                            movie.Genre();
+                            break;
+                        case MovieCategoryList.SciFi:
+                            Console.WriteLine("You entered the SciFi movie category. The list of movies are: ");
+                            movie = new SciFi();
+                            movie.Genre();
+                            break;
+                        default:
+                            break;
+                    }
                 }
-            } else if (!isMovieList)
+                else if (!isMovieList)
                 {
-                Console.WriteLine("Please enter in correct type of movie.");
-            }
+                    Console.WriteLine("You entered in an incorrect movie type.");
+                }
+            
+                Console.WriteLine("Would you like to continue? y/n: ");
+                    string continueAnswer = Console.ReadLine();
+
+                    char.TryParse(continueAnswer, out char contAnswer);
+
+                if (contAnswer == 'y' || contAnswer == 'Y')
+                {
+                    ansYes = true;
+                }
+                else
+                {
+                    ansYes = false;
+                }
+                
+            } while (ansYes == true);
+
+            Console.WriteLine("Thanks for playing! Good-bye!");
         }
     }        
 }
