100a61167011c2c877a02111d7513c0744423571
Shari <shares_322@yahoo.com>
2020-09-11 13:51:39 -0400

    Initial Commit

---
+++ TDDLab/ROSHAMBO_old.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDLab
+{
+    public class ROSHAMBO
+    {
+        public string PaperWins(string userInput1, string userInput2)
+        {
+            return userInput2;
+        }
+        public string RockWins(string userInput1, string userInput3)
+        {
+            return userInput1;
+        }
+        public string ScissorsWin(string userInput2, string userInput3)
+        {
+            return userInput3;
+        }
+    }
+}
